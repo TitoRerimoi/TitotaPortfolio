d8c8c25b1a78c16f02a42f3f73dbaaece9e81130
Rotich <tito.rotich@strathmore.edu>
2024-07-18 12:43:29 +0300

    ok

--- Photography Portfolio/main.js
+++ Photography Portfolio/main.js
@@ -1,1 +1,11 @@
-main.js
+const menuBtn = document.getElementById("menu-btn")
+const navLinks = document.getElementById("nav-links")
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen?"ri-close-line" :"ri-menu-line");
+    
+})
