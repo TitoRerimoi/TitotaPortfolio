b07f1b507446eeff944d21ace633385fbb08ba75
Rotich <tito.rotich@strathmore.edu>
2024-07-18 13:03:48 +0300

    ok

--- Photography Portfolio/main.js
+++ Photography Portfolio/main.js
@@ -7,5 +7,10 @@ menuBtn.addEventListener("click", (e) => {
 
     const isOpen = navLinks.classList.contains("open");
     menuBtnIcon.setAttribute("class", isOpen?"ri-close-line" :"ri-menu-line");
-    
-})
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+
+});
